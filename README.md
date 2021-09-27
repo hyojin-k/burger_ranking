4e034fc7cf761ed764a2cec1293066d6fff776c7
hyojin-k <hyo.khj@gmail.com>
2021-09-27 10:13:16 +0900

    final
    final

--- README.md
+++ README.md
@@ -1,1 +1,118 @@
-# burger_ranking
+# 미니 프로젝트 <BurgerRanking>
+  
+# Team
+  이병관, 김효진, 박상수, 오성현
+  
+# Stack
+  * Python
+  * Flask
+  * MongoDB
+  * JWT
+  * Html, css, Jquery
+  
+  # What is BurgerRanking
+  > 당신의 버거는 어떤 버거인가요? 그리고 다른 사람들이 좋아하는 버거는 무엇일까요?
+  
+  # Wire Frame
+  ![](https://images.velog.io/images/fnrkp089/post/a24e0b89-b9e9-41a5-ba09-15bcb8a2f9f2/KakaoTalk_20210913_145859026.jpg)
+  
+  ## 💡필요한 API
+  
+  > **회원가입 api**  
+  <span style="color:olivedrab">POST</span>  
+  **URL :**  /register  
+  **request :** {'id':id, 'password':password, 'name':name}  
+  **response :** '회원가입 완료 메세지'  
+
+  > **로그인 api**  
+  <span style="color:olivedrab">POST</span>  
+  **URL :** /login  
+  **request :** {'id':id, 'password':password}  
+  **response :** '로그인 완료/실패메세지'  
+
+  > **버거조회 api**  
+  **<span style="color:indianred">GET</span>**  
+  **URL :** /mainpage  
+  **request : ** {'burger_id':burger_id ,'name':name, 'like':like, 'dislike': dislike, 'status':status}  
+  **response :** '버거에 대한 상세 정보 리스트 표시'  
+
+  > **리뷰작성 api**  
+  **<span style="color:olivedrab">POST</span>**  
+  **URL :**/comment  
+  **request :**{'burger_id':burger_id, 'user':user', 'comment':comment}  
+  **response :** '로그인 한 유저만 리뷰 작성'  
+  
+  > **리뷰삭제 api**  
+  **<span style="color:olivedrab">POST</span>**  
+  **URL :**/comment_delete  
+  **request :**{'burger_id':burger_id, 'comment_id':comment_id', 'user':user}  
+  **response :** '해당 리뷰 작성자일경우에만 삭제'  
+
+  >**좋아요 api**  
+  **<span style="color:olivedrab">POST</span>**  
+  **URL :**/like  
+  **request :**{'burger_id':burger_id, 'like':like}  
+  **response :** '좋아요한 버거의 메세지'  
+
+  ***
+  # 화면 구성
+  ![](https://images.velog.io/images/fnrkp089/post/f32d9751-9c6c-492c-9845-dee450dbf158/%EB%B2%84%EA%B1%B01.PNG)  
+  ***로그인 페이지***  
+    
+  
+  ![](https://images.velog.io/images/fnrkp089/post/9bdbd0e2-4fb8-4f56-b148-e2e6947d8e29/%EB%B2%84%EA%B1%B02.PNG)  
+  ***회원 가입 페이지***  
+  
+  ![](https://images.velog.io/images/fnrkp089/post/a9c5025c-8cef-449f-bfe2-d3a847fca4f4/%EB%B2%84%EA%B1%B03.gif)  
+  ***버튼클릭, 리뷰작성 모습(33fps라 화질이 깨짐)***  
+ 
+  ## 관련 링크
+  https://youtu.be/Z0_9t-phu5E  
+    
+  # Solved Problems
+  > 버거데이터 베이스에 필요한 속성들은 무엇으로 할것인가?
+  * 각 브랜드별 상위 10개의 버거를 추린다.
+  * 해당 버거의 이름, 설명, 좋아요, 버거의 브랜드,  고유 버거 아이디를 추가한다.  
+    
+  ***
+  > 버거별 조회를 어떻게 할것인가?
+  * 버거의 브랜드를 담아줄 전역변수를 하나 설정하여, 해당 변수에 브랜드를 넣어준다.
+  * 브랜드가 담겨있지 않은채 실행하면 모든 버거를 가져오게하는 이벤트로 인식하게 한다.  
+    
+  ***
+  > 리뷰를 보여주는 버튼을 어떻게 할것인가?
+  * 버거 조회시 해당 이벤트에 관한 html을 같이 append 시킨다
+  * for문을 통해 배열의 index에 따라 동작하므로 id값에 index를 추가한다
+  * 특정 버거를 눌렀을 경우 id값의 index를 통해 해당 위치의 리뷰만 열리게 한다.  
+    
+  ***
+  > 리뷰 작성을 어떻게 구현할것인가?
+  * 리뷰버튼에 해당 버거의 아이디 값을 부여한다.
+  * 누를 경우 버거의 아이디와 현재 사용자의 아이디와 현재 리뷰의 인덱스 값을 넘긴다
+  * 리뷰 작성이 완료될 경우 사용자의 아이디와 버거의 아이디, 그리고 리뷰의 고유 아이디 값을 생성하여 서버로 넘긴다  
+    
+  
+  ***
+  > 리뷰 삭제를 어떻게 구현할 것인가?
+  * 현재 접속한 사용자의 아이디를 비교한다.
+  * 작성된 리뷰와 사용자의 아이디가 동일할 경우 해당 div에 '삭제' 버튼이 보이도록 한다.  
+    
+  ***
+  > 리뷰 삭제시 어떤 리뷰인지 어떻게 구분할 것인가?
+  * ObjectId Type을 String으로 형변환 후 클라이언트로 전송한다
+  * 클라이언트는 해당 Object Type을 비교하여 올바른 요청일 경우 삭제 요청을 전송한다
+  * 해당 리뷰의 고유 아이디로 조회 후, 삭제한다.
+    
+  ***
+  > 좋아요를 어떻게 구현할 것인가?
+  * 버거의 이름을 조회하여 해당 버거의 좋아요 숫자를 증가
+  * 브랜드를 담아준 전역변수를 비교하여 사용자가 눌렀던 브랜드,혹은 전체 버거를 재조회한다.  
+    
+  ***
+  > 사용자가 현재 어떤 브랜드를 눌렀는지 보여주게 할것인가?
+  * 첫 화면에서는 모든 브랜드를 조회하는 버튼에만 색을 부여
+  * 버튼 클릭마다 버튼에 각각 Addclass와 removeClass를 사용하여 css 변화  
+    
+  
+  
+  
--- app.py
+++ app.py
@@ -1,49 +1,152 @@
-from flask import Flask, render_template, jsonify, request
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from datetime import datetime, timedelta
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static"
+
+SECRET_KEY = 'SPARTA'
+
 
 client = MongoClient('localhost', 27017)
-db = client.burger_list
+db = client.project
 
+#페이지 접속시 로그인 여부 확인
 @app.route('/')
 def home():
-    return render_template('index.html')
+    #쿠키로 부터 토큰 가져오기
+    token_receive = request.cookies.get('mytoken')
+    #JWT 디코드하여 paylod에 정보 저장
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
+        #로그인 정보로 사용자의 아이디를 가져옴
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('index.html', user_info=user_info['username'])
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+#로그인 페이지 접속시
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+#회원가입 API
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    name_receive = request.form['name_give']
+    #입력받은 비밀번호를 해시값으로 저장.
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,
+        "password": password_hash,
+        "profile_name": name_receive
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+#아이디 중복확인 체크 API
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+#로그인 후, 버거 조회 API
 @app.route('/mainpage', methods=['GET'])
 def burgers_list():
-    burgers = list(db.hamburger.find({},{'_id':False}).sort('like', -1))
-    return jsonify({'result':'success', 'all_burgers': burgers})
+    #어떤 브랜드의 버거를 가져올지 'TPYE'이란 변수명에 담겨진 값을 가져와 비교
+    target = request.args.get('type');
+
+    #만일 브랜드 버거가 없는. 즉 모든 버거를 가져오라는 이벤트일 경우 모두 가져오고 그렇지 않을경우 브랜드를 조회하여 가져온다.
+    if target is not None:
+        burgers = list(db.hamburger.find({'brand': target}, {'_id': False}).sort('like', -1))
+    else:
+        burgers = list(db.hamburger.find({}, {'_id': False}).sort('like', -1))
+    return jsonify({'result': 'success', 'all_burgers': burgers})
+
+
+# 리뷰 불러오기
+@app.route('/get_comment', methods=['GET'])
+def comment_list():
+    #해당하는 버거의 고유아이디와 고유 코멘트를 가져온다.
+    burgerid = request.args.get('burgerComment')
+    comments = list(db.review.find({'burger_id': burgerid}))
+    for comment in comments:
+        comment["_id"] = str(comment["_id"])
+    return jsonify({'result': 'success', 'commentList': comments})
 
 
 # 리뷰작성
 @app.route('/comment', methods=['POST'])
 def burgers_review():
+    try:
+        #입력받은 코멘트와, 사용자의 아이디 그리고 해당되는 버거 아이디를 배열에 저장하고 해당 배열을 데이터 베이스에 입력한다.
+        comment_receive = request.form['comment_give']
+        burgerId = request.form['burgerId']
+        username = request.form['username']
+        doc = {
+            'burger_id': burgerId,
+            'username': username,
+            'comment': comment_receive,
+        }
+        db.review.insert_one(doc)
+        return jsonify({'result': 'success'})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+
+# 리뷰 삭제
+@app.route('/comment_delete', methods=['POST'])
+def review_delete():
+    #해당 리뷰를 작성한 작성자일 경우, 코멘트의 고유아이디값을 조회해 해당 이벤트가 일어난 코멘트값만 삭제한다.
     comment_receive = request.form['comment_give']
+    db.review.delete_one({'_id': ObjectId(comment_receive)})
+    return jsonify({'result': 'success'})
 
-    db.hamburger.insert_one(comment_receive)
-
-    return jsonify({'result':'success'})
 
 # 좋아요
 @app.route('/like', methods=['POST'])
 def burgers_like():
+    #이벤트가 발생한 버거의 이름으로 조회하여 해당 버거의 좋아요 갯수를 1 증가시킨다.
     name_receive = request.form['name_give']
-    like = db.hamburger.find_one({'name':name_receive})
-    new_like = like['like']+1
+    like = db.hamburger.find_one({'name': name_receive})
+    new_like = like['like'] + 1
 
-    db.hamburger.update_one({'name':name_receive}, {'$set':{'like': new_like}})
-    return jsonify({'result':'success'})
-
-# 싫어요
-@app.route('/dislike', methods=['POST'])
-def burgers_dislike():
-    name_receive = request.form['name_give']
-    dislike = db.hamburger.find_one({'name': name_receive})
-    new_dislike = dislike['dislike'] + 1
-
-    db.hamburger.update_one({'name': name_receive}, {'$set': {'dislike': new_dislike}})
+    db.hamburger.update_one({'name': name_receive}, {'$set': {'like': new_like}})
     return jsonify({'result': 'success'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
